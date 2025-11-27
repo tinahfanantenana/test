282dbff7ce9db5f060bcfc386c6dd41996f19a56
tinahfanantenana <tinahfanantenanaratahiriniaina@gmail.com>
2025-11-27 14:04:30 +0300

    mDM1

--- app.js
+++ app.js
@@ -3,16 +3,17 @@
 // // ul.setAttribute("hidden","hidden");
 // console.log(ul.getAttribute("class"));
 // setInterval(()=>ul.classList.toggle('red'),1000);
-const li=document.querySelector('ul li:first-child');
+// const li=document.querySelector('ul li:first-child');
 // const li=lis.classList
 // setInterval(()=>li.toggle('red'),1000); 
 // console.log(li.style.color='blue')
 // console.log(getComputedStyle(li).color);
-const newLi= document.createElement('li')
-newLi.innerHTML= "Bonjour"
-document.querySelector('ul').append(newLi); //prepend met au debut des contenu ou éément enfant de l'element;
-const ul=document.querySelector('ul');
-const div= document.createElement('div');
-div.innerHTML="Bonjour les gens"
-ul.insertAdjacentElement('afterbegin', div);
+// const newLi= document.createElement('li')
+// newLi.innerHTML= "Bonjour"
+// document.querySelector('ul').append(newLi); //prepend met au debut des contenu ou éément enfant de l'element;
+// const ul=document.querySelector('ul');
+// const div= document.createElement('div');
+// div.innerHTML="Bonjour les gens"
+// ul.insertAdjacentElement('afterbegin', div);
+const lastP=document.querySelector('div.lastPosts');
 
