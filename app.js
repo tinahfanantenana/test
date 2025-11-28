9cb5be26debf68711125490ba5b5fe780942091a
tinahfanantenana <tinahfanantenanaratahiriniaina@gmail.com>
2025-11-28 05:53:16 +0100

    DOM

--- app.js
+++ app.js
@@ -15,5 +15,50 @@
 // const div= document.createElement('div');
 // div.innerHTML="Bonjour les gens"
 // ul.insertAdjacentElement('afterbegin', div);
-const lastP=document.querySelector('div.lastPosts');
+const lastP=document.querySelector('#lastPosts');
+// lastP.insertAdjacentHTML('beforebegin','<h2>Loading...</h2>');
+const divPost= document.createElement('div');
+lastP.append(divPost);
+await fetch('https://jsonplaceholder.typicode.com/posts/?_limit=5',{
+    headers:{
+        Accept:'application/json'
+    }
+});   
+
+await setTimeout(()=>{},3000);
+
+
+
+const postApi= fetch('https://jsonplaceholder.typicode.com/posts/?_limit=5',{
+    headers:{
+        Accept:'application/json'
+    }
+});   
+postApi
+.then(response=>response.json())
+.then(posts=>{
+setTimeout(()=>{
+    document.querySelector('h2').remove();
+    for([property, value] of Object.entries(posts)){
+        const postDiv=document.createElement('div');
+        postDiv.innerHTML=`
+        <h2>${value.title}</h2>
+        <p>${value.body}</p>
+        `;
+        divPost.append(postDiv);
+    }
+    lastP.append(divPost);
+    console.log(posts);
+},3000)
+    // posts.forEach(post=>{
+    //     const postDiv=document.createElement('div');
+    //     postDiv.innerHTML=`
+    //     <h2>${post.title}</h2>
+    //     <p>${post.body}</p>
+    //     `;
+    //     divPost.appendChild(postDiv);
+    // });
+    })
+.catch(error=>console.log('erreur :'+ error.message));
+
 
