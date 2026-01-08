a252be41d9beba9689c96ea10f5ac0eab055d176
tinahfanantenana <tinahfanantenanaratahiriniaina@gmail.com>
2026-01-09 00:29:41 +0300

    scrollspypc50

--- scrollspy/modules/Scrollspy.js
+++ scrollspy/modules/Scrollspy.js
@@ -1,3 +1,33 @@
 export class ScrollSpy{
-    
+    contentSelector;
+    linkSelector;
+    #ratio=0.6;
+    constructor(contentSelector,linkSelector){
+        this.linkSelector=linkSelector;
+        this.contentSelector=contentSelector;
+    }
+
+    activeMenu(){
+        let y=math.round(window.innerHeight*this.#ratio)
+        const observer= new IntersectionObserver(
+            (entries)=>{
+                entries.forEach(entrie => {
+                    if(entrie.intersectionRatio>0){
+                        const id=entrie.target.getAttribute('id');
+                        this.#activeLink(id);
+                    }
+                });
+
+                if(length.this.contentSelector>1){
+                    observer.observe(this.contentSelector)
+                }
+            }
+            ,
+            {rootMargin: `${window.innerHeight-y-1}px 0px -${y}px 0px`}
+        )
+    }
+
+    #activeLink(){
+
+    }
 }
