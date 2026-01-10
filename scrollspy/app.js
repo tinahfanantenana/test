49327680db5fe4a24177ef4689e02a9ae3d34df2
tinahfanantenana <tinahfanantenanaratahiriniaina@gmail.com>
2026-01-10 12:14:22 +0300

    scrollspyok

--- scrollspy/app.js
+++ scrollspy/app.js
@@ -1,10 +1,19 @@
 import {ViewRenderer} from './components/ViewRenderer.js';
+import { ScrollSpy } from './modules/Scrollspy.js';
+import { debounce } from './utils/debounce.js';
 
  const container= document.querySelector('.container');
  const view= new ViewRenderer();
- const navbar= view.createElement('nav', {class: "nav"});
+ const navbar= view.createElement('nav', {class: "nav nav-tabs navbar-expand-lg navbar-light bg-light fixed-top"});
  await view.loadTemplate(navbar,'../components/navbar.html');
  const div= view.createElement('div', {class:"content"});
  await view.loadTemplate(div,'../components/content.html');
  container.append(navbar);
  container.append(div);
+
+const contents=document.querySelector('.content');
+const nav=document.querySelector('.nav');
+
+let observer=new ScrollSpy(contents,nav);
+observer.activeMenu();
+window.addEventListener('resize',debounce(function(){observer.activeMenu()},500));
--- scrollspy/modules/Scrollspy.js
+++ scrollspy/modules/Scrollspy.js
@@ -1,3 +1,6 @@
+/**
+ * objet pour gérer le scrollspy en affichage
+ */
 export class ScrollSpy{
     contentSelector;
     linkSelector;
@@ -7,27 +10,47 @@ export class ScrollSpy{
         this.contentSelector=contentSelector;
     }
 
+    /**
+     * fonction qui sert à activé un menu selon l'affichage
+     */
     activeMenu(){
-        let y=math.round(window.innerHeight*this.#ratio)
-        const observer= new IntersectionObserver(
-            (entries)=>{
-                entries.forEach(entrie => {
-                    if(entrie.intersectionRatio>0){
-                        const id=entrie.target.getAttribute('id');
-                        this.#activeLink(id);
-                    }
-                });
-
-                if(length.this.contentSelector>1){
-                    observer.observe(this.contentSelector)
-                }
-            }
-            ,
-            {rootMargin: `${window.innerHeight-y-1}px 0px -${y}px 0px`}
-        )
+        let y=Math.round(window.innerHeight*this.#ratio)
+        const spies=this.contentSelector.querySelectorAll('[data-content]')
+        if(spies.length>0){
+            const observer= new IntersectionObserver(
+                (entries)=>this.#callback(entries)
+                ,
+                {rootMargin: `-${window.innerHeight-y-1}px 0px -${y}px 0px`}
+            )
+            spies.forEach( spy => {
+                observer.observe(spy)
+            }); 
+        }
     }
 
-    #activeLink(){
+    /**
+     * 
+     * @param {HTMLElement} element 
+     */
+    #activeLink(element){
+        const id=element.getAttribute('id');
+        const link=this.linkSelector.querySelector(`a[href="#${id}"]`);
+        if (link === null){
+            return null
+        }
+        link.parentElement.querySelectorAll('.active').forEach(node=>node.classList.remove('active'));
+        link.classList.add('active');
+    }
 
+    /**
+     * 
+     * @param {IntersectionObserverEntry[]} entries 
+     */
+    #callback(entries){
+        entries.forEach(entrie => {
+            if(entrie.intersectionRatio>0){;
+                this.#activeLink(entrie.target);
+            }
+        });
     }
 }
