0df88ff934f6ec37db1b7f3ca340d5c5d4dbc207
tinahfanantenana <tinahfanantenanaratahiriniaina@gmail.com>
2026-01-20 09:56:49 +0300

    commentmvcavance

--- comments/cores/InfinitePagination.js
+++ comments/cores/InfinitePagination.js
@@ -1,3 +1,35 @@
+import { ApiService } from "../services/ApiService";
 export class InfinitePagination{
+    #onLoad;
+    #onError;
+    #loading;
+    #element;
+    #observer;
+    constructor({element, onLoad, onError}){
+        this.#element=element;
+        this.#onLoad=onLoad;
+        this.#onError=onError;
+        this.#observer=new IntersectionObserver((entries)=>{
+            entries.forEach(entrie => {
+                if(entrie.isIntersecting){
+                    this.#loadMore();
+                }
+            });
+        })
+        this.#observer.observe(this.#element);
+    }
 
+    async #loadMore(){
+        if (this.#loading){
+            return
+        }
+        
+        try {
+            this.#loading=true;
+            this.#onLoad();
+            this.#loading=false;
+        } catch (error) {
+            this.#onError();
+        }
+    }
 }
