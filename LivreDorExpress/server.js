7f177e5e5ddc097b2311c537f8f9cb29a1cfab75
tinahfanantenana <tinahfanantenanaratahiriniaina@gmail.com>
2026-04-24 16:22:59 +0300

    livreor

--- LivreDorExpress/server.js
+++ LivreDorExpress/server.js
@@ -2,8 +2,12 @@ import express from 'express';
 
 const app= express();
 
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
 
 app.get('/', (req, resp)=>{
-    resp.send('heloo word');
+    resp.render('pages/index',{test:"salut"});
 })
+
 app.listen(8000)
