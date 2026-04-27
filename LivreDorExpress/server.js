8671668eb3d262b63c7d97f5b7daf5db637c37f3
tinahfanantenana <tinahfanantenanaratahiriniaina@gmail.com>
2026-04-27 07:15:55 +0300

    LivreDorsesssioninit

--- LivreDorExpress/server.js
+++ LivreDorExpress/server.js
@@ -1,18 +1,39 @@
     import express from 'express';
+    import session from 'express-session';
 
     const app= express();
 
     app.set('view engine', 'ejs');
+    if (process.env.NODE_ENV === 'production') {
+        app.set('trust proxy', 1);
+    };
 
     app.use(express.static('public'));
-    app.use(express.urlencoded())
-    app.use(express.json())
+    app.use(express.urlencoded());
+    app.use(express.json());
+    app.use(session({
+        secret: 'secret',
+        resave: false,
+        saveUninitialized: true,
+        cookie: { secure: process.env.NODE_ENV === 'production' }
+    }));
 
+    
     app.get('/', (req, resp)=>{
+        console.log(req.session.error);
+        resp.locals.error=req.session.error;
+        req.session.error=undefined;
         resp.render('pages/index');
-    })
+    });
 
     app.post('/',(req,resp)=>{
-        console.log(req.body);
-    })
-    app.listen(8000)
+        if(req.body.message===''|| req.body.message===undefined){
+            req.session.error='il y a une errreur'
+            resp.redirect('/');
+        }else{
+            console.log(req.body.message);
+            resp.redirect('/');
+        }
+    });
+
+    app.listen(8000);
